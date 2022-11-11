3dde087b7828fdfb07675ce29e955ded02178439
Livia <liviafausto03@gmail.com>
2022-11-11 15:12:42 -0300

    Operadores: aprendendo operador ternário

---
+++ SINTAXE-JAVA/src/edu/livia/operadores/OperadorTernario.java
@@ -0,0 +1,18 @@
+package edu.livia.operadores;
+
+public class OperadorTernario {
+    // <expressão condicional> ? <caso seja true> : <caso seja false>
+
+    public static void main(String[] arg){
+        int a = 6, b = 5;
+        int resultado;
+
+        //String resultado = a==b? "verdadeiro" : "falso";
+
+        resultado = a==b? 1 : 0;
+
+        System.out.println(resultado);
+
+    }
+    
+}
