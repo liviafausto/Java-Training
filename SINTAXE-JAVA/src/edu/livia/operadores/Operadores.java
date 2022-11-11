4f62b5fa5796154e85bfae414c0620104f87bf6e
Livia <liviafausto03@gmail.com>
2022-11-11 12:55:09 -0300

    Operadores: aprendendo concatenação de strings em Java

--- SINTAXE-JAVA/src/edu/livia/operadores/Operadores.java
+++ SINTAXE-JAVA/src/edu/livia/operadores/Operadores.java
@@ -1,5 +1,12 @@
 package edu.livia.operadores;
 
 public class Operadores {
+
+    public static void main(String[] args){
+        String nomeCompleto = "LINGUAGEM" + " " + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+    }
     
 }
