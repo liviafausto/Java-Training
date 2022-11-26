fb2ffe66841c2c1a756655e1603e6762fee786f8
Livia <liviafausto03@gmail.com>
2022-11-26 18:39:49 -0300

    Update: organização de diretórios

--- COLLECTIONS/src/edu/livia/list/Ex01.java
+++ COLLECTIONS/src/edu/livia/list/Ex01.java
@@ -1,13 +1,13 @@
-/*
-Faça um programa que receba a temperatura média dos 6 primeiros meses do ano e armazene-as em uma lista.
-Após isto, calcule a média semestral das temperaturas e mostre todas as temperaturas acima desta média, e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro e etc).
-*/
 package edu.livia.list;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+
+/*
+Faça um programa que receba a temperatura média dos 6 primeiros meses do ano e armazene-as em uma lista.
+Após isto, calcule a média semestral das temperaturas e mostre todas as temperaturas acima desta média, e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro e etc).
+*/
 
 public class Ex01 {
 
