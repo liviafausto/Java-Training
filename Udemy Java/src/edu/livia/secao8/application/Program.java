8ef188ab1b3d8619eca94f2fa0f785a7a3e6d43c
Livia <liviafausto03@gmail.com>
2022-12-27 18:19:03 -0300

    Curso Udemy: segundo problema de exemplo adicionado

--- Udemy Java/src/edu/livia/secao8/application/Program.java
+++ Udemy Java/src/edu/livia/secao8/application/Program.java
@@ -1,6 +1,7 @@
 package edu.livia.secao8.application;
 
-import edu.livia.secao8.entities.Triangle;
+//import edu.livia.secao8.entities.Triangle;
+import edu.livia.secao8.entities.Product;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,7 +10,32 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner read = new Scanner(System.in);
 
-        Triangle x, y;
+        Product product = new Product();
+
+        System.out.println("Enter product data:");
+        System.out.print("Name: ");
+        product.name = read.nextLine();
+        System.out.print("Price: ");
+        product.price = read.nextDouble();
+        System.out.print("Quantity: ");
+        product.quantity = read.nextInt();
+
+        System.out.println("Product data: " + product);
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        product.addProducts(read.nextInt());
+        System.out.println("Updated data: " + product);
+
+        System.out.print("Enter the number of products to be removed from stock: ");
+        product.removeProducts(read.nextInt());
+        System.out.println("Updated data: " + product);
+
+        read.close();
+    }
+}
+
+// Primeiro exemplo: calcular as áreas dos triângulos
+/*      Triangle x, y;
         x = new Triangle();
         y = new Triangle();
 
@@ -17,6 +43,7 @@ public class Program {
         x.ladoA = read.nextDouble();
         x.ladoB = read.nextDouble();
         x.ladoC = read.nextDouble();
+
         System.out.println("Enter the measures of triangle Y:");
         y.ladoA = read.nextDouble();
         y.ladoB = read.nextDouble();
@@ -32,6 +59,4 @@ public class Program {
             System.out.println("Larger area: X");
         } else {
             System.out.println("Larger area: Y");
-        }
-    }
-}
+        }                                                   */
