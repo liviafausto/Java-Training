50f9c6ec0e1b4b599302d9159128c422ef0ed4b6
Livia <liviafausto03@gmail.com>
2022-12-27 18:49:14 -0300

    Seção 8: exercício 1, retangulo

--- Udemy Java/src/edu/livia/secao8/application/Program.java
+++ Udemy Java/src/edu/livia/secao8/application/Program.java
@@ -1,7 +1,9 @@
 package edu.livia.secao8.application;
 
 //import edu.livia.secao8.entities.Triangle;
-import edu.livia.secao8.entities.Product;
+//import edu.livia.secao8.entities.Product;
+
+import edu.livia.secao8.entities.Rectangle;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,26 +11,15 @@ public class Program {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner read = new Scanner(System.in);
+        Rectangle rectangle = new Rectangle();
 
-        Product product = new Product();
+        System.out.println("Enter rectangle width and height:");
+        rectangle.width = read.nextDouble();
+        rectangle.height = read.nextDouble();
 
-        System.out.println("Enter product data:");
-        System.out.print("Name: ");
-        product.name = read.nextLine();
-        System.out.print("Price: ");
-        product.price = read.nextDouble();
-        System.out.print("Quantity: ");
-        product.quantity = read.nextInt();
-
-        System.out.println("Product data: " + product);
-
-        System.out.print("Enter the number of products to be added in stock: ");
-        product.addProducts(read.nextInt());
-        System.out.println("Updated data: " + product);
-
-        System.out.print("Enter the number of products to be removed from stock: ");
-        product.removeProducts(read.nextInt());
-        System.out.println("Updated data: " + product);
+        System.out.printf("AREA = %.2f\n", rectangle.area());
+        System.out.printf("PERIMETER = %.2f\n", rectangle.perimeter());
+        System.out.printf("DIAGONAL = %.2f\n", rectangle.diagonal());
 
         read.close();
     }
@@ -60,3 +51,24 @@ public class Program {
         } else {
             System.out.println("Larger area: Y");
         }                                                   */
+
+// Segundo exemplo: ler e alterar dados de um produto em estoque
+/*      Product product = new Product();
+
+        System.out.println("Enter product data:");
+        System.out.print("Name: ");
+        product.name = read.nextLine();
+        System.out.print("Price: ");
+        product.price = read.nextDouble();
+        System.out.print("Quantity: ");
+        product.quantity = read.nextInt();
+
+        System.out.println("Product data: " + product);
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        product.addProducts(read.nextInt());
+        System.out.println("Updated data: " + product);
+
+        System.out.print("Enter the number of products to be removed from stock: ");
+        product.removeProducts(read.nextInt());
+        System.out.println("Updated data: " + product);                             */
